d362a23ec121d0209def67ce33abadec5ffcdcb8
GastónVidart <gaston.vidart@gmail.com>
2023-01-22 19:07:55 -0300

    agregado buscador gifs

--- giffy/src/pages/Home/index.js
+++ giffy/src/pages/Home/index.js
@@ -1,15 +1,38 @@
-import React from "react";
-import { Link } from "wouter";
+import React, { useState } from "react";
+import { Link, useLocation } from "wouter";
 
 const POPULAR_GIFS = ["pandas", "matrix", "transformers"];
 
 export default function Home() {
+  const [keyword, setKeyword] = useState("");
+  const [path, pushLocation] = useLocation();
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    pushLocation(`/search/${keyword}`)
+  };
+
+  const handleInputChange = (evt) => {
+    setKeyword(evt.target.value);
+  };
   return (
     <>
-      <h3>Los gifs más popuulares</h3>
+      <form onSubmit={handleSubmit}>
+        <div style={{ display: "flex", gap: "16px", alignItems: "center" }}>
+          <label htmlFor="buscador">Buscador:</label>
+          <input
+            id="buscador"
+            style={{ height: "auto" }}
+            type="text"
+            value={keyword}
+            onChange={handleInputChange}
+          ></input>
+        </div>
+      </form>
+      <h3>Los gifs más populares</h3>
       <ul>
         {POPULAR_GIFS.map((popularGif) => (
-          <li key={popularGif} style={{listStyle:"none"}}>
+          <li key={popularGif} style={{ listStyle: "none" }}>
             <Link to={`/search/${popularGif}`}>Gifs de {popularGif}</Link>
           </li>
         ))}
