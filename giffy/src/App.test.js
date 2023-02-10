8e9f02da0f9d427362cba65c166fd264601d0846
GastónVidart <gaston.vidart@gmail.com>
2023-02-10 20:09:05 -0300

    Agregado test sincrono

--- giffy/src/App.test.js
+++ giffy/src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test("La app renderiza!", () => {
+  /*render(<App />);
+  const linkElement = screen.getByText(/Última Búsqueda/i);*/
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Última Búsqueda/i);
   expect(linkElement).toBeInTheDocument();
 });
