dd977a844955fa3330597c3e0ebf2cc2d0201746
GastónVidart <gaston.vidart@gmail.com>
2023-02-12 19:46:40 -0300

    Agregado isLoading, isError en fetch singleGif y página error 404

--- giffy/src/hooks/useSingleGif.js
+++ giffy/src/hooks/useSingleGif.js
@@ -6,18 +6,26 @@ export function useSingleGif(gifId) {
   const gifs = useGlobalGifs();
 
   const [gif, setGif] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
-    const gifFromCache = gifs.find((gif) => gif.id === gifId);
+    setIsLoading(true);
 
+    const gifFromCache = gifs.find((gif) => gif.id === gifId);
     if (!gifFromCache) {
-      getSingleGif(gifId).then((gif) => {
-        setGif(gif);
-      });
+      getSingleGif(gifId)
+        .then((gif) => {
+          setGif(gif);
+          setIsError(false);
+        })
+        .catch((e) => setIsError(true))
+        .finally(() => setIsLoading(false));
     } else {
       setGif(gifFromCache);
+      setIsLoading(false);
     }
   }, [gifId]);
 
-  return gif;
+  return { gif, isLoading, isError };
 }
--- giffy/src/pages/Detail/index.js
+++ giffy/src/pages/Detail/index.js
@@ -2,9 +2,15 @@ import React from "react";
 
 import Gif from "components/Gif";
 import { useSingleGif } from "hooks/useSingleGif";
+import Spinner from "components/Spinner";
+import { Redirect } from "wouter";
 
 export default function GifDetails({ params }) {
-  const gif = useSingleGif(params.gifId);
+  const { gif, isLoading, isError } = useSingleGif(params.gifId);
+
+  if (isLoading) return <Spinner />;
+
+  if (isError) return <Redirect to="/404" />;
 
   return Boolean(gif) ? (
     <Gif title={gif.title} id={gif.id} url={gif.url}></Gif>
